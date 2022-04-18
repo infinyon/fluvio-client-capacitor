d20385ecf2d5b07092344f6df09ba3229fbfb47d
Nick Cardin <nick@cardin.email>
2022-04-18 17:13:04 -0500

    files

--- src/web.ts
+++ src/web.ts
@@ -3,7 +3,7 @@ import { WebPlugin } from '@capacitor/core';
 import type { FluvioPlugin } from './definitions';
 
 export class FluvioWeb extends WebPlugin implements FluvioPlugin {
-  async produce(options: { value: string; }): Promise<{ value: string; }> {
+  async produce(_options: { value: string; }): Promise<{ value: string; }> {
     throw new Error('Method not implemented.');
   }
   async connect(options: { value: string }): Promise<{ value: string }> {

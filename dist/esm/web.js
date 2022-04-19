cb6d9fa6493449e59dc608630a819108bd2cd808
Nick Cardin <nick@cardin.email>
2022-04-19 17:05:18 -0500

    add action workflows

--- dist/esm/web.js
+++ dist/esm/web.js
@@ -1,5 +1,8 @@
 import { WebPlugin } from '@capacitor/core';
 export class FluvioWeb extends WebPlugin {
+    async produce(_options) {
+        throw new Error('Method not implemented.');
+    }
     async connect(options) {
         console.log('ECHO', options);
         return options;
--- example/vite.config.js
+++ example/vite.config.js
@@ -1,3 +1,3 @@
 export default {
-    root: 'www'
-}
+  root: 'www',
+};
